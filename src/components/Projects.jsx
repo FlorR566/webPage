2b7048df2c97671346f851cc7d5d7e10d7fcfa4a
FlorR566 <florodriguez566@gmail.com>
2025-12-12 12:33:12 -0300

    Agrego nuevo proyecto en desarrollo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,29 +9,52 @@ const Projects = () => {
           <div className="project-card">
             <a
               className="project-a"
+              href="https://ladyvalentina.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/src/lady-valentina.png"
+                alt="Lady Valentina App"
+                width="270"
+                height="150"
+              />
+
+              <h3>Lady Valentina</h3>
+              <p>
+                Landing page para un comercio de carteras, desarrollada con
+                React, JavaScript y CSS. Diseño responsive adaptado a
+                dispositivos móviles. Proyecto actualmente en desarrollo.
+              </p>
+            </a>
+          </div>
+
+          <div className="project-card">
+            <a
+              className="project-a"
               href="https://florr566.github.io/RandomUser-Grapher/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h3>RandomUser Explorer</h3>
-              <p>
-                Herramienta para obtener datos de usuarios random, mediante una
-                API. Incluye un gráfico que muestra la cantidad de mujeres y
-                hombres.
-              </p>
-
               <img
                 src="/src/random-user-explorer-lightMode.png"
                 alt="random-user-explorer-lightMode"
                 width="180"
                 height="150"
               />
-              <img
+              {/* <img
                 src="/src/random-user-explorer-darkMode.png"
                 alt="random-user-explorer-darkMode"
                 width="150"
                 height="150"
-              />
+              /> */}
+
+              <h3>RandomUser Explorer</h3>
+              <p>
+                Herramienta para obtener datos de usuarios random, mediante una
+                API. Incluye un gráfico que muestra la cantidad de mujeres y
+                hombres.
+              </p>
             </a>
           </div>
 
@@ -42,17 +65,17 @@ const Projects = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h3>Weather App</h3>
-              <p>
-                Aplicación para consultar el clima en tiempo real: temperatura,
-                humedad y más, consumiendo una API externa.
-              </p>
               <img
                 src="/src/weather-app.png"
                 alt="Weather-app"
                 width="270"
                 height="150"
               />
+              <h3>Weather App</h3>
+              <p>
+                Aplicación para consultar el clima en tiempo real: temperatura,
+                humedad y más, consumiendo una API externa.
+              </p>
             </a>
           </div>
 
@@ -63,18 +86,17 @@ const Projects = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h3>To-Do List</h3>
-              <p>
-                Aplicación interactiva para gestionar tareas, con almacenamiento
-                local, desarrollada en JavaScript, CSS y HTML.
-              </p>
-
               <img
                 src="/src/to-do-list.png"
                 alt="To-do-list"
                 width="270"
                 height="150"
               />
+              <h3>To-Do List</h3>
+              <p>
+                Aplicación interactiva para gestionar tareas, con almacenamiento
+                local, desarrollada en JavaScript, CSS y HTML.
+              </p>
             </a>
           </div>
         </div>
