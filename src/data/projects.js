b2140c784a73b79136dcfb988a42bdf8f912a5ea
FlorR566 <florodriguez566@gmail.com>
2025-12-16 19:33:57 -0300

    Separar los ptoyectos en un archivo distinto

---
+++ src/data/projects.js
@@ -0,0 +1,41 @@
+import img1 from '/src/lady-valentina.png';
+import img2 from '/src/random-user-explorer-lightMode.png';
+import img3 from '/src/weather-app.png';
+import img4 from '/src/to-do-list.png';
+
+const PROJECTS = [
+  {
+    id: 1,
+    src: img1,
+    alt: 'Lady Valentina App',
+    title: 'Lady Valentina',
+    text: 'Landing page para un comercio de carteras, desarrollada con React, JavaScript y CSS. Diseño responsive adaptado a dispositivos móviles. Proyecto actualmente en desarrollo.',
+    href: 'https://ladyvalentina.vercel.app/',
+  },
+  {
+    id: 2,
+    src: img2,
+    alt: 'Random-user-explorer-lightMode',
+    title: 'RandomUser Explorer',
+    text: 'Herramienta para obtener datos de usuarios random, mediante una API. Incluye un gráfico que muestra la cantidad de mujeres y hombres.',
+    href: 'https://florr566.github.io/RandomUser-Grapher/',
+  },
+  {
+    id: 3,
+    src: img3,
+    alt: 'Weather App',
+    title: 'Weather App',
+    text: 'Aplicación para consultar el clima en tiempo real: temperatura, humedad y más, consumiendo una API externa.',
+    href: 'https://florr566.github.io/Weather-App-FR/',
+  },
+  {
+    id: 4,
+    src: img4,
+    alt: 'To-Do List',
+    title: 'To-Do List',
+    text: 'Aplicación interactiva para gestionar tareas, con almacenamiento local, desarrollada en JavaScript, CSS y HTML.',
+    href: 'https://florr566.github.io/TO-DO-List/',
+  },
+];
+
+export default PROJECTS;
