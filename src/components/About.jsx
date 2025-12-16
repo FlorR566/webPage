73a156b99634150b987bc1d886cbe8ac122ff77b
FlorR566 <florodriguez566@gmail.com>
2025-12-16 12:59:39 -0300

    Separar about text del componente About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,31 @@
 import '../styles.css';
 
+const ABOUT_TEXT = (
+  <>
+    Hola! Soy Florencia Rodriguez, Desarrolladora Frontend en formación con
+    enfoque en <strong>React</strong>. Actualmente curso la
+    <strong> Tecnicatura Universitaria en Programación</strong> en la
+    Universidad Tecnológica Nacional
+    <strong> (UTN)</strong>.
+    <br />
+    <br />
+    Trabajo con componentes, estados, hooks, manejo de formularios y consumo de
+    APIs usando fetch o axios. También manejo <strong>HTML</strong>,{' '}
+    <strong>CSS</strong>, <strong>JavaScript</strong>, <strong>Python</strong> y
+    herramientas como <strong>Git/ GitHub</strong>, <strong>npm</strong> y{' '}
+    <strong>Json-server</strong>.
+    <br />
+    Me interesa seguir mejorando como desarrolladora frontend y aplicar buenas
+    prácticas en cada proyecto.
+  </>
+);
+
 const About = () => {
   return (
-    <div>
-      <section id="about" className="about">
-        <h2>Sobre mí</h2>
-        <p>
-          Actualmente curso la{' '}
-          <strong>Tecnicatura Universitaria en Programación</strong> en la{' '}
-          <strong>Universidad Tecnológica Nacional (UTN)</strong>.
-          <br />
-          Complemento mi formación con cursos y desarrollo de proyectos
-          personales utilizando
-          <strong> React</strong>.
-          <br />
-          <br />
-          Trabajo con conceptos como componentes, estados, hooks, manejo de
-          formularios y consumo de APIs usando fetch o axios. También manejo{' '}
-          <strong>HTML</strong>, <strong>CSS</strong>,{' '}
-          <strong>JavaScript</strong>, <strong>Python</strong> y herramientas
-          como <strong>Git/GitHub</strong>, <strong>npm</strong> y{' '}
-          <strong>json-server</strong>.
-          <br />
-          Me interesa seguir mejorando como desarrolladora frontend y aplicar
-          buenas prácticas en cada proyecto.
-        </p>
-      </section>
-    </div>
+    <section id="about" className="about">
+      <h2>Sobre mí</h2>
+      <p>{ABOUT_TEXT}</p>
+    </section>
   );
 };
 
