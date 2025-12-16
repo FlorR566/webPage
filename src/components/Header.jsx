72d1b7ae5923b630061226dc847fdacab335a6e0
FlorR566 <florodriguez566@gmail.com>
2025-12-16 18:46:30 -0300

    Eliminar Div innecesario

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,24 @@ import '../styles.css';
 
 const Header = () => {
   return (
-    <div>
-      <header>
-        <nav>
-          <div id="inicio" className="logo">
-            <a href="#hero">Florencia Rodriguez 🌸🐝</a>
-          </div>
-          <ul className="nav-links">
-            <li>
-              <a href="#about">Sobre mí</a>
-            </li>
-            <li>
-              <a href="#projects">Proyectos</a>
-            </li>
-            <li>
-              <a href="#contact">Contacto</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <header>
+      <nav>
+        <div id="inicio" className="logo">
+          <a href="#hero">Florencia Rodriguez 🌸🐝</a>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <a href="#about">Sobre mí</a>
+          </li>
+          <li>
+            <a href="#projects">Proyectos</a>
+          </li>
+          <li>
+            <a href="#contact">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
